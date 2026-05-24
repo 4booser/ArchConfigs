d1a6974a8dc1a21d2b41d890d551b57ae655f741
4booser <4booser@gmail.com>
2026-05-24 14:04:54 +0300

    Configure Hyprland dual monitors

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -226,10 +226,17 @@ hl.window_rule({
 })
 
 hl.monitor({
-    output = "HDMI-A-1",
-    mode = "highrr",
-    position = "0x0",
-    scale = 1,
+ output = "DP-3",
+ mode = "2560x1440@144",
+ position = "0x0",
+ scale = 1,
+})
+
+hl.monitor({
+ output = "HDMI-A-1",
+ mode = "1920x1080@74.97",
+ position = "2560x0",
+ scale = 1,
 })
 
 hl.layer_rule({
